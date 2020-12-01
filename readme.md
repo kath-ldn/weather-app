285ba83a73820ac87d96f9298fe1f103e7e0ea26
Kath <katharineturner3@gmail.com>
2020-12-01 15:34:58 +0000

    First commit V1

---
+++ readme.md
@@ -0,0 +1,9 @@
+You should be able to toggle displaying the data in Fahrenheit or Celsius.
+
+Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API.
+Push that baby to github and share your solution below!
+
+For future versions:
+- Add loading screen
+- Validate cities - if not a city, tell me, and ensure correct city/country is identified.
+- Improve style/CSS
---
+++ script.js
@@ -0,0 +1,145 @@
+function updateImage(conditionData, h){
+    let conditionImg = document.getElementById("conditionImg");
+    let condition = conditionData.toLowerCase();
+    if(condition.includes("clear")) {
+        if(h > 6 && h < 18) {
+            conditionImg.src = "/icons/sun-icon.png";
+        } else {
+        conditionImg.src = "/icons/moon-icon.png";
+        }
+    } else if (condition.includes("clouds") && !condition.includes("rain") && !condition.includes("drizzle")) {
+        conditionImg.src = "/icons/clouds-icon.png";
+    } else if (condition.includes("rain") || condition.includes("drizzle") && !condition.includes("thunderstorm")) {
+        conditionImg.src = "/icons/rain-icon.png";
+    } else if (condition.includes("thunderstorm")) {
+        conditionImg.src = "/icons/thunderstorm-icon.png";
+    } else if (condition.includes("snow")) {
+        conditionImg.src = "/icons/snow-icon.png";
+    } else if (condition.includes("mist" || condition.includes("smoke") || condition.includes("haze") || condition.includes("sand") || condition.includes("fog") || condition.includes("dust") || condition.includes("ash"))) {
+        conditionImg.src = "/icons/fog-icon.png";
+    } else if (condition.includes("qualls" || "tornado")) {
+        conditionImg.src = "/icons/wind-icon.png";
+    }
+
+}
+
+function getUKTime(timezone) {
+    let tempDate = new Date()
+    let tempHours = tempDate.getUTCHours();  
+    let tempMins = tempDate.getUTCMinutes();
+    return totalSeconds = ((tempHours * 60) + tempMins) * 60;
+}
+
+function calculateLocationTime(locationSeconds, conditionData) {
+    let time = document.getElementById("time");
+    locationSeconds = Number(locationSeconds);
+    var h = Math.floor(locationSeconds / 3600);
+    var m = Math.floor(locationSeconds % 3600 / 60);
+
+    if(h >= 24) {
+        h = h - 24;
+    }
+
+    let amPM;
+    if(h < 12) {
+        amPM = "am";
+    } else {
+        amPM = "pm";
+    }
+
+    if (m >= 10) {
+    time.textContent = h + ":" + m + amPM;
+    } else {
+        time.textContent = h + ":" + "0" + m + amPM;
+    }
+    updateImage(conditionData, h);
+}
+
+function getCurrentTime(timezone, conditionData) {
+    let UKseconds = getUKTime(timezone);
+    let locationSeconds = UKseconds + timezone;
+    calculateLocationTime(locationSeconds, conditionData)
+}
+
+function updatePage(temp, feelsLike, name, country, conditionData, timezone) {
+    //id's divs to hold info
+    let cityName = document.getElementById("cityName");
+    let countryName = document.getElementById("countryName");
+    let cityTemp = document.getElementById("cityTemp");
+    let cityTempFeels = document.getElementById("cityTempFeels");
+    let condition = document.getElementById("condition");
+    //add if-else for temp
+    //updates
+    cityName.textContent = name;
+    countryName.textContent = country;
+    cityTemp.textContent = temp;
+    cityTempFeels.textContent = feelsLike;
+    condition.textContent = conditionData;
+    getCurrentTime(timezone, conditionData);
+}
+
+// uses async/await function to get weather deets
+    async function getWeather(unitUrl) {
+    let citySearch = document.getElementById("search");        
+      const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+      //add if-else for temp
+      let userCity = search.userText.value;
+      const response = await fetch(baseUrl + userCity + unitUrl, {mode: 'cors'});
+      const weatherData = await response.json();
+      const temp = weatherData.main.temp;
+      const feelsLike = weatherData.main.feels_like;
+      const name = weatherData.name;
+      const country = weatherData.sys.country;
+      const conditionData = weatherData.weather[0].description;
+      const timezone = weatherData.timezone;
+      updatePage(temp, feelsLike, name, country, conditionData, timezone);
+    } 
+
+let unitSwitch = document.getElementById("unitSwitch");
+
+function getWeatherInUnit() {
+    if (unitSwitch.checked) {
+        unitUrl = '&units=imperial&appid=762516bd2937e6133ccabeabe77788c1';
+            getWeather(unitUrl)
+    } else {
+        unitUrl = '&units=metric&appid=762516bd2937e6133ccabeabe77788c1';
+        getWeather(unitUrl)
+    }
+}
+
+//button to submit user selection
+    let submit = document.getElementById("submit");
+      search.addEventListener("click", () =>{
+          getWeatherInUnit();
+          event.preventDefault();
+      });
+
+
+
+unitSwitch.addEventListener("change", function() {
+    if (this.checked) {
+        getWeatherInUnit();
+      } else {
+        getWeatherInUnit();
+      }
+});
+
+/* For reference - example using promises rather than async/await
+uses promises to get weather deets
+    function promiseWeather() {
+        const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=762516bd2937e6133ccabeabe77788c1'
+        fetch(baseUrl, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            console.log(response.main.temp)
+        })
+        .catch(function(err) {
+            console.log("Oh no", err)
+        })
+    }
+promiseWeather();
+
+const { getTime } = require("date-fns");
+*/
